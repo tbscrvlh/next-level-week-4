5adf07ea6b9ea14b179c43243160b6a003bbfbf6
Tobias Carvalho <tobias.jung.carvalho@gmail.com>
2021-02-22 15:26:27 -0300

    fixing bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ExperienceBar } from "./components/ExperienceBar";
 
-import "./styles/global.css";
+import './styles/global.css'
 
 function App() {
   return (
--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -3,7 +3,7 @@ export function ExperienceBar() {
         <header className="experience-bar">
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }} ></div>
+                <div style={{ width: '50%' }} />
                 <span className="current-experience" style={{ left: '50%' }}>
                     300 xp
                 </span>
